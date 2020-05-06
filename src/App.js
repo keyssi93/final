6fc9129c3300022e2956662bc13e882e8a9966c9
keyssi93 <keyssi.arroyo@gmail.com>
2020-05-05 20:37:58 -0500

    otro

--- src/App.js
+++ src/App.js
@@ -4,31 +4,57 @@ import  './data.js';
 import {today,hotelsData} from './data.js';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       filters: {
-        dateFrom: today,
+        dateFrom: today, 
         dateTo: new Date(today.valueOf() + 86400000),
         country: '',
-        price: 0,
-        rooms: 0
+        price: '',
+        rooms: ''
       },
       hotels: hotelsData
     };
+    this.handleOptionChange=this.handleOptionChange.bind(this)
+    this.filterHotels=this.filterHotels.bind(this)
   }
+  
+  handleOptionChange(payload) {
+    this.setState({
+      filters: payload
+    })
+  }
+
+  filterHotels(hotel) {
+    const { filters } = this.state;
+    if ((filters.dateFrom && filters.dateFrom > hotel.availabilityFrom)
+      || (filters.dateTo && filters.dateTo< hotel.availabilityTo)
+      || (filters.country && filters.country !== hotel.country)
+      || (filters.price && filters.price !== hotel.price)
+      || (filters.rooms === 10 && hotel.rooms > 10)
+      || (filters.rooms === 20 && (hotel.rooms <= 10 || hotel.rooms > 20))
+      || (filters.rooms === 30 && hotel.rooms <= 20)) return false;
+
+    return true;
+  }
+
+
   render() {
+
     return (
       <div>
-        <Hero filters={this.state.filters} />
-        <Filters filters={this.state.filters}  onFilterChange={ this.handleFilterChange } />
-        <Hotel hotel={ hotelsData[0] } />
-        <Hotels data={hotelsData} />
+        <Hero filters={ this.state.filters } />
+        <Filters
+          filters={ this.state.filters }
+          onFilterChange={ this.handleFilterChange }
+        />
+        <Hotels data={this.state.hotels.filter(this.filterHotels)} />      
       </div>
-    );    
+    )
   }
-}
-class Hero extends App{
+  }
+class Hero extends React.Component {
   render(){
   const formato = {weekday: "long",
   year: "numeric", 
@@ -41,14 +67,14 @@ class Hero extends App{
       <div className="container">
         <h1 className="title">Hoteles</h1>
         <h2 className="subtitle">
-        desde el <strong>{this.state.filters.dateFrom.toLocaleString("es-ES", formato)}</strong> hasta el <strong>{this.state.filters.dateTo.toLocaleString("es-ES", formato)}</strong>
+        desde el <strong onChange={this.handledatechange}>{this.props.filters.dateFrom.toLocaleString("es-ES", formato)}</strong> hasta el <strong>{this.props.filters.dateTo.toLocaleString("es-ES", formato)}</strong>
         </h2> 
       </div>
     </div>
   </section>
   )
 }}
-class DateFilter extends App {
+class DateFilter extends React.Component {
   constructor(props){super(props)
   this.handledatechange =this.handledatechange.bind(this)}
   handledatechange(event){
@@ -64,19 +90,25 @@ class DateFilter extends App {
   </div>
 </div>)}
 }
-class OptionsFilter extends App { 
-
-
+class OptionsFilter extends React.Component { 
+ 
   render() { 
+   
   return(
   <div className="field">
   <div className="control has-icons-left">
     <div className="select" style={ {width: '100%'} }>
-      <select style={ {width: '100%'} }>
-       <ul>
-         {this.props.options.map((options)=>{
-           return <Filters name={options} key={options}/>})}
-       </ul>
+      <select onChange={this.handleFilterChange} style={ {width: '100%'} }>
+    {this.props.options.map(option=>{
+      return(
+        <option key={option.name} value={option.value}>
+          {option.name}
+        </option>
+      )
+    }
+
+    )}
+      
         
       </select>
     </div>
@@ -86,7 +118,7 @@ class OptionsFilter extends App {
   </div>
 </div>)}
 }
-class Filters extends App{
+class Filters extends React.Component{
   handleOptionChange(event) {
     let payload = this.props.filters;
     payload[event.target.name] = event.target.value;
@@ -97,12 +129,12 @@ class Filters extends App{
 <nav className="navbar is-info" style={ {justifyContent: 'center'} }>
   <div className="navbar-item">
     <DateFilter
-      date={ this.state.filters.dateFrom}
+      date={ this.props.filters.dateFrom}
       icon="sign-in-alt" />
   </div>
   <div className="navbar-item">
     <DateFilter
-      date={ this.state.filters.dateTo }
+      date={ this.props.filters.dateTo }
       icon="sign-out-alt" />
   </div>
   <div className="navbar-item">
@@ -114,7 +146,7 @@ class Filters extends App{
   <div className="navbar-item">
     <OptionsFilter
       options={ [ {value: undefined, name: 'Cualquier precio'}, {value: 1, name: '$'}, {value: 2, name: '$$'}, {value: 3, name: '$$$'}, {value: 4, name: '$$$$'} ] }
-      selected={ this.state.filters.price }
+      selected={ this.props.filters.price }
       icon="dollar-sign" />
   </div>
   <div className="navbar-item">
@@ -123,34 +155,38 @@ class Filters extends App{
       {value: 10, name: 'Hotel pequeño'},
       {value: 20, name: 'Hotel mediano'},
       {value: 30, name: 'Hotel grande'} ] }
-      selected={ this.state.filters.rooms }
+      selected={ this.props.filters.rooms }
       icon="bed" />
   </div>
 </nav>
   )
 }}
-function Hotel(){
+class Hotel extends React.Component {
+  
+  
+   render(){
+    const { data } = this.props;
   return (
     <div className="card">
   <div className="card-image">
     <figure className="image is-4by3">
-      <img src="./images/sainte-jeanne.jpg" alt="Sainte Jeanne Boutique & Spa" />
+      <img src={data.photo} alt="foto" />
     </figure>
   </div>
   <div className="card-content">
-    <p className="title is-4">Sainte Jeanne Boutique & Spa</p>
-    <p>Sainte Jeanne Hotel Boutique & Spa está ubicado en el corazón de Los Troncos, un barrio residencial y elegante de Mar del Plata. El lujo, el confort y la pasión por los detalles dan personalidad a esta cálida propuesta.</p>
+  <p className="title is-4">{data.name}</p>
+  <p>{data.description}</p>
     <div className="field is-grouped is-grouped-multiline" style={{marginTop: '1em'}}>
       <div className="control">
       <div className="tags has-addons">
           <span className="tag is-medium is-info"><i className="fas fa-map-marker"></i></span>
-          <span className="tag is-medium">Mar del Plata, Argentina</span>
+  <span className="tag is-medium">{data.city},{data.country} </span>
         </div>
       </div>
       <div className="control">
         <div className="tags has-addons">
           <span className="tag is-medium is-info"><i className="fas fa-bed"></i></span>
-          <span className="tag is-medium">23 Habitaciones</span>
+          <span className="tag is-medium">{data.rooms} Habitaciones</span>
         </div>
       </div>
       <div className="control">
@@ -165,23 +201,43 @@ function Hotel(){
       </div>
     </div>
   </div>
+  <div className="card-footer">
+  <a href="javascript:alert('No implementamos esto aún :(')" className="card-footer-item has-background-primary has-text-white has-text-weight-bold">Reservar</a>
 </div>
+</div>
+ 
   )
-}
-function Hotels(props) {
-  const listhotel = hotelsData.map((hotel) =>
-  <div className="column is-one-third">
-  <Hotel data={ hotel } />
-</div>
-);
+}}
+
+
+
+class Hotels extends React.Component {
+ 
+render(){
+  
 return (
   <section className="section" style={ {marginTop: '3em'} }>
-  <div className="container">
-    <div className="columns is-multiline">
-    <i className="fas">{listhotel}</i>
-    </div>
-  </div>
+  {hotelsData.length 
+    ? (
+      <div className="container">
+        <div className="columns is-multiline">
+          {hotelsData.map(hotels => (
+            <div key={ hotels.slug } className="column is-one-third">
+              <Hotel data={ hotels } />
+            </div>
+            ))}
+        </div>
+      </div>
+    )
+    : (
+      <article className="message is-warning">
+        <div className="message-body">
+          No se han encontrado hoteles que coincidan con los parámetros de búsqueda.
+        </div>
+      </article>
+    )
+  }
 </section>
 )
-}
+}}
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,12 +1,32 @@
 // Definimos los datos estáticos que nuestra aplicación utilizará para funcionar.
 // En una aplicación convencional estos datos provendrían desde un back-end pero a los fines de este ejercicio utilizaremos variables pre-definidas.
 // Utilizamos fechas dinámicas para que la aplicación siempre muestre un rango de días entre la fecha actual y el futuro.
-const today=new Date();
-const hotelsData = [
+import img1 from './images/la-bamba-de-areco.jpg'
+import img2 from './images/sainte-jeanne.jpg'
+import img3 from './images/entre-cielos.jpg'
+import img4 from './images/huacalera.jpg'
+import img5 from './images/merced-del-alto.jpg'
+import img6 from './images/azur-real.jpg'
+import img7 from './images/rincon-del-socorro.jpg'
+import img8 from './images/luma-casa-de-montana.jpg'
+import img9 from './images/casa-turquesa.jpg'
+import img10 from './images/vila-da-santa.jpg'
+import img11 from './images/uxua-casa.jpg'
+import img12 from './images/ponta-dos-ganchos.jpg'
+import img13 from './images/alto-atacama.jpg'
+import img14 from './images/tierra-patagonia.jpg'
+import img15 from './images/vira-vira.jpg'
+import img16 from './images/vik-chile.jpg'
+import img17 from './images/casa-higueras.jpg'
+import img18 from './images/campo-tinto.jpg'
+
+
+export const today = new Date()
+export const hotelsData = [
   {
     slug: 'la-bamba-de-areco',
     name: 'La Bamba de Areco',
-    photo: './images/la-bamba-de-areco.jpg',
+    photo: img1,
     description: 'La Bamba de Areco está ubicada en San Antonio de Areco, en el corazón de la pampa. Es una de las estancias más antiguas de la Argentina, recientemente restaurada para ofrecer a sus huéspedes todo el confort y esplendor colonial.',
     availabilityFrom: today.valueOf(),
     availabilityTo: today.valueOf() + 864000000, // 10 days
@@ -18,7 +38,7 @@ const hotelsData = [
   {
     slug: 'sainte-jeanne',
     name: 'Sainte Jeanne Boutique & Spa',
-    photo: './images/sainte-jeanne.jpg',
+    photo: img2,
     description: 'Sainte Jeanne Hotel Boutique & Spa está ubicado en el corazón de Los Troncos, un barrio residencial y elegante de Mar del Plata. El lujo, el confort y la pasión por los detalles dan personalidad a esta cálida propuesta.',
     availabilityFrom: today.valueOf() + 864000000, // 10 days
     availabilityTo: today.valueOf() + 1296000000, // 15 days
@@ -30,7 +50,7 @@ const hotelsData = [
   {
     slug: 'entre-cielos',
     name: 'Entre Cielos',
-    photo: './images/entre-cielos.jpg',
+    photo: img3,
     description: 'Ubicado en una de las regiones vitivinícolas más grandes de América Latina, Entre Cielos fue pensado y construido en un campo de 8 hectáreas rodeado de viñedos malbec y una imponente vista de la Cordillera de Los Andes.',
     availabilityFrom: today.valueOf() + 432000000, // 5 days
     availabilityTo: today.valueOf() + 1296000000, // 15 days
@@ -42,7 +62,7 @@ const hotelsData = [
   {
     slug: 'huacalera',
     name: 'Hotel Huacalera',
-    photo: './images/huacalera.jpg',
+    photo: img4,
     description: 'Esta casona neocolonial, construida en la década de 1940, está ubicada en el corazón de la Quebrada de Humahuaca, un extenso valle rodeado de imponentes cadenas montañosas, recorrido por el Río Grande y declarado Patrimonio de la Humanidad en 2003.',
     availabilityFrom: today.valueOf() + 1728000000, // 20 days
     availabilityTo: today.valueOf() + 2592000000, // 30 days
@@ -54,7 +74,7 @@ const hotelsData = [
   {
     slug: 'merced-del-alto',
     name: 'La Merced del Alto',
-    photo: './images/merced-del-alto.jpg',
+    photo: img5,
     description: 'Al pie del Nevado de Cachi, La Merced del Alto se destaca sobre el pintoresco valle rodeado de cerros, ríos y arroyos. Dominando sobre lo alto, el hotel y sus imponentes vistas invitan a explorar los Valle Calchaquíes o simplemente a disfrutar de la paz del lugar.',
     availabilityFrom: today.valueOf(),
     availabilityTo: today.valueOf() + 432000000, // 5 days
@@ -66,7 +86,7 @@ const hotelsData = [
   {
     slug: 'azur-real',
     name: 'Azur Real Hotel',
-    photo: './images/azur-real.jpg',
+    photo: img6,
     description: 'La exclusividad rodeada de historia. Azur Real Hotel Boutique está ubicado en el corazón de la zona comercial y el centro histórico de Córdoba, dentro de uno de los principales circuitos culturales y turísticos de la ciudad.',
     availabilityFrom: today.valueOf() + 1296000000, // 15 days 
     availabilityTo: today.valueOf() + 2592000000, // 30 days
@@ -78,7 +98,7 @@ const hotelsData = [
   {
     slug: 'rincon-del-socorro',
     name: 'Rincón del Socorro',
-    photo: './images/rincon-del-socorro.jpg',
+    photo: img7,
     description: 'Rincón del Socorro es una estancia ubicada en la reserva natural de los Esteros del Iberá, un santuario de vida silvestre, donde la Fundación Conservation Land Trust decidió desarrollar un ambicioso proyecto de recuperación y conservación.',
     availabilityFrom: today.valueOf() + 432000000, // 5 days
     availabilityTo: today.valueOf() + 1296000000, // 15 days
@@ -90,7 +110,7 @@ const hotelsData = [
   {
     slug: 'luma-casa-de-montana',
     name: 'Luma Casa de Montaña',
-    photo: './images/luma-casa-de-montana.jpg',
+    photo: img8,
     description: 'Emplazada en un entorno natural a orillas del lago Nahuel Huapi y con la imponente imagen de la Cordillera de Los Andes, Luma Casa de Montaña se presenta majestuosa pero a la vez simple y hogareña: un lugar diferente, atemporal y mágico.',
     availabilityFrom: today.valueOf(),
     availabilityTo: today.valueOf() + 1296000000, // 15 days
@@ -102,7 +122,7 @@ const hotelsData = [
   {
     slug: 'casa-turquesa',
     name: 'Casa Turquesa',
-    photo: './images/casa-turquesa.jpg',
+    photo: img9,
     description: 'Casa Turquesa es una histórica mansión del siglo XVIII que recrea el encanto típico de Paraty, una de las herencias coloniales más bellas de Brasil, declarada Patrimonio Mundial de la Humanidad por la UNESCO.',
     availabilityFrom: today.valueOf(),
     availabilityTo: today.valueOf() + 432000000, // 5 days
@@ -114,7 +134,7 @@ const hotelsData = [
   {
     slug: 'vila-da-santa',
     name: 'Vila Da Santa',
-    photo: './images/vila-da-santa.jpg',
+    photo: img10,
     description: 'Esta casa de pescadores fue renovada con elegancia, pero sin descuidar su espíritu original. Se abre a un gran patio central con dos piscinas de diseño, una de ellas climatizada.',
     availabilityFrom: today.valueOf() + 864000000, // 10 days
     availabilityTo: today.valueOf() + 1296000000, // 15 days
@@ -126,7 +146,7 @@ const hotelsData = [
   {
     slug: 'uxua-casa',
     name: 'UXUA Casa Hotel & Spa',
-    photo: './images/uxua-casa.jpg',
+    photo: img11,
     description: 'UXUA Casa Hotel & Spa es un hotel boutique cinco estrellas reconocido mundialmente por su incomparable belleza tropical, el maravilloso bar con vista al mar, el galardonado restaurante, un gimnasio totalmente equipado y el inigualable spa Almescar que ofrece innovadores tratamientos con ingredientes extraídos de la selva.',
     availabilityFrom: today.valueOf(),
     availabilityTo: today.valueOf() + 864000000, // 10 days
@@ -138,7 +158,7 @@ const hotelsData = [
   {
     slug: 'ponta-dos-ganchos',
     name: 'Ponta dos Ganchos',
-    photo: './images/uxua-casa.jpg',
+    photo: img12,
     description: 'Ubicado a pasos de San Pablo, Río de Janeiro, e incluso Buenos Aires, en una península privada y rodeado por un pintoresco pueblo de pescadores, se encuentra Ponta dos Ganchos, uno de los resorts de playa más exclusivos del sur de Brasil.',
     availabilityFrom: today.valueOf() + 432000000, // 5 days
     availabilityTo: today.valueOf() + 864000000, // 10 days
@@ -150,7 +170,7 @@ const hotelsData = [
   {
     slug: 'alto-atacama',
     name: 'Alto Atacama',
-    photo: './images/alto-atacama.jpg',
+    photo: img13,
     description: 'Alto Atacama Desert Lodge & Spa es un distinguido refugio ubicado a los pies del Pukará de Quitor en el imponente desierto de Atacama al norte de Chile.',
     availabilityFrom: today.valueOf(),
     availabilityTo: today.valueOf() + 864000000, // 10 days
@@ -162,7 +182,7 @@ const hotelsData = [
   {
     slug: 'tierra-patagonia',
     name: 'Tierra Patagonia',
-    photo: './images/tierra-patagonia.jpg',
+    photo: img14,
     description: 'Tierra Patagonia Hotel & Spa es un lujoso hotel boutique ubicado en la Patagonia chilena a orillas del lago Sarmiento, envuelto en el extraordinario escenario natural de Torres del Paine, un parque nacional rodeado de montañas, cascadas, glaciares, lagos y peñascos, declarado Reserva de la Biosfera por la UNESCO en 1978.',
     availabilityFrom: today.valueOf() + 2592000000, // 30 days
     availabilityTo: today.valueOf() + 3456000000, // 40 days
@@ -174,7 +194,7 @@ const hotelsData = [
   {
     slug: 'vira-vira',
     name: 'Vira Vira',
-    photo: './images/vira-vira.jpg',
+    photo: img15,
     description: 'Hotel Hacienda Vira Vira Relais & Chateaux se encuentra en una ubicación privilegiada, muy cerca de Pucón, Chile. Su impresionante parque nativo de 23 hectáreas a orillas del río Liucura ofrece un oasis único de paz y tranquilidad.',
     availabilityFrom: today.valueOf() + 864000000, // 10 days
     availabilityTo: today.valueOf() + 1728000000, // 20 days
@@ -186,7 +206,7 @@ const hotelsData = [
   {
     slug: 'vik-chile',
     name: 'Vik Chile',
-    photo: './images/vik-chile.jpg',
+    photo: img16,
     description: 'Vik Chile es una hacienda moderna y sofisticada en donde confluyen el arte y el diseño de vanguardia, una ubicación sin igual, fabulosos escenarios naturales, servicio personalizado y una cuidada atención por el medioambiente.',
     availabilityFrom: today.valueOf() + 432000000, // 5 days
     availabilityTo: today.valueOf() + 1728000000, // 20 days
@@ -198,7 +218,7 @@ const hotelsData = [
   {
     slug: 'casa-higueras',
     name: 'Casa Higueras',
-    photo: './images/casa-higueras.jpg',
+    photo: img17,
     description: 'Ubicada en el corazón del Cerro Alegre, frente a la imponente bahía de Valparaíso, se encuentra Casa Higueras, el primer hotel boutique y de diseño del puerto.',
     availabilityFrom: today.valueOf() + 432000000, // 5 days
     availabilityTo: today.valueOf() + 1296000000, // 15 days
@@ -210,7 +230,7 @@ const hotelsData = [
   {
     slug: 'campo-tinto',
     name: 'Campo Tinto',
-    photo: './images/campo-tinto.jpg',
+    photo: img18,
     description: 'Campo Tinto es una chacra de 25 hectáreas ubicada en el corazón de San Roque, en medio del ondulante Carmelo, una zona de chacras, viñedos, bodegas y frutales, con mucha historia y un ritmo de vida tranquilo y natural.',
     availabilityFrom: today.valueOf() + 2160000000, // 25 days
     availabilityTo: today.valueOf() + 2592000000, // 30 days
@@ -220,5 +240,3 @@ const hotelsData = [
     price: 1
   }
 ]
-
-module.exports={today,hotelsData};
